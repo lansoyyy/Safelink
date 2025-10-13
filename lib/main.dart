69d0b505a8712ffb7e5e1f86a91e882ab77bed81
Lans12345 <olanalans12345@gmail.com>
2025-10-14 07:42:56 +0800

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,24 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:safelink/firebase_options.dart';
 import 'package:safelink/screens/auth/splash_screen.dart';
 import 'package:safelink/screens/dashboard/dashboard_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    name: 'safelink-60c18',
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  // Set system UI overlay style
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.light,
+    ),
+  );
+
   runApp(const MyApp());
 }
 
